b8460415636baa2977ee8e6df00e39a844964541
Heiko Kromm <heiko@kromm.net>
2025-12-28 21:56:36 +0100

    Add Timer Values

--- components/truma_inetbox/sensor/__init__.py
+++ components/truma_inetbox/sensor/__init__.py
@@ -17,6 +17,7 @@ from esphome.const import (
     UNIT_EMPTY,
     ICON_GAS_CYLINDER,
     ICON_POWER,
+    ICON_CLOCK, # Neu hinzugefügt
 )
 from .. import truma_inetbox_ns, CONF_TRUMA_INETBOX_ID, TrumaINetBoxApp
 
@@ -28,7 +29,6 @@ CONF_CLASS = "class"
 TrumaSensor = truma_inetbox_ns.class_(
     "TrumaSensor", sensor.Sensor, cg.Component)
 
-# `TRUMA_SENSOR_TYPE` is a enum class and not a namespace but it works.
 TRUMA_SENSOR_TYPE_dummy_ns = truma_inetbox_ns.namespace("TRUMA_SENSOR_TYPE")
 
 CONF_SUPPORTED_TYPE = {
@@ -87,13 +87,37 @@ CONF_SUPPORTED_TYPE = {
         CONF_UNIT_OF_MEASUREMENT: UNIT_EMPTY,
         CONF_ACCURACY_DECIMALS: 0,
     },
+    "TIMER_START_TIME": {
+        CONF_CLASS: TRUMA_SENSOR_TYPE_dummy_ns.TIMER_START_TIME,
+        CONF_UNIT_OF_MEASUREMENT: UNIT_EMPTY,
+        CONF_ICON: ICON_CLOCK,
+        CONF_ACCURACY_DECIMALS: 0,
+    },
+    "TIMER_STOP_TIME": {
+        CONF_CLASS: TRUMA_SENSOR_TYPE_dummy_ns.TIMER_STOP_TIME,
+        CONF_UNIT_OF_MEASUREMENT: UNIT_EMPTY,
+        CONF_ICON: ICON_CLOCK,
+        CONF_ACCURACY_DECIMALS: 0,
+    },
+    "TIMER_ROOM_TEMPERATURE": {
+        CONF_CLASS: TRUMA_SENSOR_TYPE_dummy_ns.TIMER_ROOM_TEMPERATURE,
+        CONF_UNIT_OF_MEASUREMENT: UNIT_CELSIUS,
+        CONF_ICON: ICON_THERMOMETER,
+        CONF_ACCURACY_DECIMALS: 0,
+        CONF_DEVICE_CLASS: DEVICE_CLASS_TEMPERATURE,
+    },
+    "TIMER_WATER_TEMPERATURE": {
+        CONF_CLASS: TRUMA_SENSOR_TYPE_dummy_ns.TIMER_WATER_TEMPERATURE,
+        CONF_UNIT_OF_MEASUREMENT: UNIT_CELSIUS,
+        CONF_ICON: ICON_THERMOMETER,
+        CONF_ACCURACY_DECIMALS: 0,
+        CONF_DEVICE_CLASS: DEVICE_CLASS_TEMPERATURE,
+    },
 }
-
 
 def set_default_based_on_type():
     def set_defaults_(config):
         sensor_type = CONF_SUPPORTED_TYPE[config[CONF_TYPE]]
-        # set defaults based on sensor type:
         if CONF_UNIT_OF_MEASUREMENT in sensor_type and CONF_UNIT_OF_MEASUREMENT not in config:
             config[CONF_UNIT_OF_MEASUREMENT] = sensor_type[CONF_UNIT_OF_MEASUREMENT]
         if CONF_ICON in sensor_type and CONF_ICON not in config:
@@ -103,26 +127,24 @@ def set_default_based_on_type():
         if CONF_DEVICE_CLASS in sensor_type and CONF_DEVICE_CLASS not in config:
             config[CONF_DEVICE_CLASS] = sensor_type[CONF_DEVICE_CLASS]
         return config
-
     return set_defaults_
 
-
+# Geändertes Schema für Kompatibilität mit ESPHome 2025
 CONFIG_SCHEMA = sensor.sensor_schema(
     state_class=STATE_CLASS_MEASUREMENT
 ).extend(
-    {
+    cv.Schema({
         cv.GenerateID(): cv.declare_id(TrumaSensor),
         cv.GenerateID(CONF_TRUMA_INETBOX_ID): cv.use_id(TrumaINetBoxApp),
         cv.Required(CONF_TYPE): cv.enum(CONF_SUPPORTED_TYPE, upper=True),
-    }
+    })
 ).extend(cv.COMPONENT_SCHEMA)
-FINAL_VALIDATE_SCHEMA = set_default_based_on_type()
 
+FINAL_VALIDATE_SCHEMA = set_default_based_on_type()
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await sensor.register_sensor(var, config)
     await cg.register_parented(var, config[CONF_TRUMA_INETBOX_ID])
-
     cg.add(var.set_type(CONF_SUPPORTED_TYPE[config[CONF_TYPE]][CONF_CLASS]))
