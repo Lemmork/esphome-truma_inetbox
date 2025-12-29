3a1a25e98d9543901b260a35b003edb66ed922f8
Heiko Kromm <heiko@kromm.net>
2025-12-29 19:31:00 +0100

    Switch to data-getters

--- components/truma_inetbox/TrumaiNetBoxAppHeater.h
+++ components/truma_inetbox/TrumaiNetBoxAppHeater.h
@@ -13,15 +13,15 @@ class TrumaiNetBoxAppHeater : public TrumaStausFrameResponseStorage<StatusFrameH
   void dump_data() const override;
   bool can_update() override;
 
-  float get_current_room_temperature() const { return (float)this->status_.current_temp_room / 10.0f; }
-  float get_current_water_temperature() const { return (float)this->status_.current_temp_water / 10.0f; }
-  float get_target_room_temperature() const { return (float)this->status_.target_temp_room / 10.0f; }
-  uint8_t get_target_water_temperature() const { return this->status_.target_temp_water; }
-  uint8_t get_heating_mode() const { return (uint8_t)this->status_.heating_mode; }
-  uint8_t get_operating_status() const { return (uint8_t)this->status_.operating_status; }
-  uint16_t get_error_code() const { return this->status_.error_code; }
-  uint8_t get_electric_power_level() const { return (uint8_t)this->status_.electric_power_level; }
-  uint8_t get_energy_mix() const { return (uint8_t)this->status_.energy_mix; }
+  float get_current_room_temperature() const { return (float)this->data_.current_temp_room / 10.0f; }
+  float get_current_water_temperature() const { return (float)this->data_.current_temp_water / 10.0f; }
+  float get_target_room_temperature() const { return (float)this->data_.target_temp_room / 10.0f; }
+  uint8_t get_target_water_temperature() const { return this->data_.target_temp_water; }
+  uint8_t get_heating_mode() const { return (uint8_t)this->data_.heating_mode; }
+  uint8_t get_operating_status() const { return (uint8_t)this->data_.operating_status; }
+  uint16_t get_error_code() const { return this->data_.error_code; }
+  uint8_t get_electric_power_level() const { return (uint8_t)this->data_.electric_power_level; }
+  uint8_t get_energy_mix() const { return (uint8_t)this->data_.energy_mix; }
 
   bool action_heater_room(uint8_t temperature, HeatingMode mode = HeatingMode::HEATING_MODE_OFF);
   bool action_heater_water(uint8_t temperature);
--- components/truma_inetbox/TrumaiNetBoxAppTimer.h
+++ components/truma_inetbox/TrumaiNetBoxAppTimer.h
@@ -12,10 +12,10 @@ class TrumaiNetBoxAppTimer : public TrumaStausFrameResponseStorage<StatusFrameTi
   void create_update_data(StatusFrame *response, uint8_t *response_len, uint8_t command_counter) override;
   void dump_data() const override;
 
-  uint16_t get_start_time() const { return this->status_.start_time; }
-  uint16_t get_stop_time() const { return this->status_.stop_time; }
-  float get_room_temperature() const { return (float)this->status_.target_temp_room / 10.0f; }
-  uint8_t get_water_temperature() const { return this->status_.target_temp_water; }
+  uint16_t get_start_time() const { return this->data_.start_time; }
+  uint16_t get_stop_time() const { return this->data_.stop_time; }
+  float get_room_temperature() const { return (float)this->data_.target_temp_room / 10.0f; }
+  uint8_t get_water_temperature() const { return this->data_.target_temp_water; }
 
   bool action_timer_disable();
   bool action_timer_activate(u_int16_t start, u_int16_t stop, uint8_t room_temperature,
